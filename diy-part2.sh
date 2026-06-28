fbb6cf4b4e8cc8cd4a132bc08b40367149facc37
wdhysrz <2918159660@qq.com>
2026-06-28 13:27:30 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# Github 镜像加速，解决云编译克隆超时
-git config --global url."https://mirror.ghproxy.com/https://github.com/".insteadOf "https://github.com/"
-
-# 拉取PassWall2依赖库（内置适配PW2的最新版 sing-box）
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
-# 拉取PassWall2 WEB管理面板
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
-
-src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages.git
-# 新增下面两行，拉取面板包到编译清单
-./scripts/feeds update passwall
+# 更新并安装passwall全套
+./scripts/feeds update passwall passwall2
 ./scripts/feeds install -a -p passwall
+./scripts/feeds install -a -p passwall2
 
-# 修改默认后台地址 192.168.1.1 → 192.168.6.1
+# 修改后台IP地址
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
