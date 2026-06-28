b62b0fa9289b9ae31aae4da8648d509f5a07da70
wdhysrz <2918159660@qq.com>
2026-06-28 10:18:19 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,5 +7,10 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git p
 # 拉取PassWall2 WEB管理面板
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 
+src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages.git
+# 新增下面两行，拉取面板包到编译清单
+./scripts/feeds update passwall
+./scripts/feeds install -a -p passwall
+
 # 修改默认后台地址 192.168.1.1 → 192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
