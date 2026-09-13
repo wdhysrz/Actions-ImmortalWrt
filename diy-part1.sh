84370445b26658a039bebf245eddd8f87153065c
wdhysrz <2918159660@qq.com>
2026-09-14 00:00:56 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,12 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/cachesize\t8000/cachesize\t0/g' package/network/services/dnsmasq/files/dhcp.conf
 #sed -i 's/mini_ttl\t3600/mini_ttl\t0/g' package/network/services/dnsmasq/files/dhcp.conf
 
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+ git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+ # 移除 openwrt feeds 过时的luci版本
+ rm -rf feeds/luci/applications/luci-app-passwall
+ git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+
 # Remove 6in4
 sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
 sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
